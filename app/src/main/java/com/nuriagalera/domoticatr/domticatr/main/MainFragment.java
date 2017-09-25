42f7ceae3823d29f7a74c8532cf9fa46331deaec
nurietta1511 <nuria88888@gmail.com>
2017-09-25 18:55:16 +0200

    crear dos botons a la pantalla principal

--- app/src/main/java/com/nuriagalera/domoticatr/domticatr/main/MainFragment.java
+++ app/src/main/java/com/nuriagalera/domoticatr/domticatr/main/MainFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.nuriagalera.domoticatr.domticatr.R;
 
@@ -29,7 +32,44 @@ public class MainFragment extends Fragment implements MainContract.View {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.main_frag, container, false);
 
+        Switch llumsWC1 = (Switch) root.findViewById(R.id.llumsWC1);
+        llumsWC1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                llumnsWc1OnClick(isChecked);
+            }
+        });
+        final Switch llummsWc2 = (Switch) root.findViewById(R.id.llumsWc2);
+        llummsWc2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                llummsWc2onClick(isChecked);
+            }
+        });
+
         return root;
+    }
+    private void llummsWc2onClick(boolean isChecked){
+        if(isChecked == true){
+            //llum oberta
+            Toast.makeText(getActivity(), "llum Wc 2 oberta!!!", Toast.LENGTH_SHORT).show();
+        }else{
+            //llum apagada
+            Toast.makeText(getActivity(), "llum Wc 2 apagada!!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void llumnsWc1OnClick(boolean isChecked) {
+
+        if(isChecked == true){
+            //llum oberta
+            Toast.makeText(getActivity(), "llum Wc 1 oberta!!!", Toast.LENGTH_SHORT).show();
+        }else{
+            //llum apagada
+            Toast.makeText(getActivity(), "llum Wc 1 apagada!!!", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
     @Override
