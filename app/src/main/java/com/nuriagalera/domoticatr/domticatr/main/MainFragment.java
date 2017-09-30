9cda17588751f4836bd3de7ba5eac65d566460ff
nurietta1511 <nuria88888@gmail.com>
2017-09-30 12:23:02 +0200

    - Creat llista pantalla principal. - Creat botons llista

--- app/src/main/java/com/nuriagalera/domoticatr/domticatr/main/MainFragment.java
+++ app/src/main/java/com/nuriagalera/domoticatr/domticatr/main/MainFragment.java
@@ -1,16 +1,24 @@
 package com.nuriagalera.domoticatr.domticatr.main;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nuriagalera.domoticatr.domticatr.R;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class MainFragment extends Fragment implements MainContract.View {
 
@@ -31,8 +39,32 @@ public class MainFragment extends Fragment implements MainContract.View {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.main_frag, container, false);
+        ListView list = (ListView) root.findViewById(R.id.list);
 
-        Switch llumsWC1 = (Switch) root.findViewById(R.id.llumsWC1);
+        // Defined Array values to show in ListView
+        String[] values = new String[]{"Dormitori principal",
+                "Dormitori secundari",
+                "Despatx",
+                "Lavabo planta principal",
+                "Lavabo planta superior",
+                "Menjador",
+                "Rebost",
+                "Cuina",
+                "Escala",
+                "Jardi"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),R.layout.list_row,R.id.listRowText,values);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selected = ((TextView) view.findViewById(R.id.listRowText)).getText().toString();
+                Toast.makeText(getContext(),selected,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        /*Switch llumsWC1 = (Switch) root.findViewById(R.id.llumsWC1);
         llumsWC1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -45,15 +77,16 @@ public class MainFragment extends Fragment implements MainContract.View {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 llummsWc2onClick(isChecked);
             }
-        });
+        });*/
 
         return root;
     }
-    private void llummsWc2onClick(boolean isChecked){
-        if(isChecked == true){
+
+    private void llummsWc2onClick(boolean isChecked) {
+        if (isChecked == true) {
             //llum oberta
             Toast.makeText(getActivity(), "llum Wc 2 oberta!!!", Toast.LENGTH_SHORT).show();
-        }else{
+        } else {
             //llum apagada
             Toast.makeText(getActivity(), "llum Wc 2 apagada!!!", Toast.LENGTH_SHORT).show();
         }
@@ -61,10 +94,10 @@ public class MainFragment extends Fragment implements MainContract.View {
 
     private void llumnsWc1OnClick(boolean isChecked) {
 
-        if(isChecked == true){
+        if (isChecked == true) {
             //llum oberta
             Toast.makeText(getActivity(), "llum Wc 1 oberta!!!", Toast.LENGTH_SHORT).show();
-        }else{
+        } else {
             //llum apagada
             Toast.makeText(getActivity(), "llum Wc 1 apagada!!!", Toast.LENGTH_SHORT).show();
         }
@@ -76,4 +109,32 @@ public class MainFragment extends Fragment implements MainContract.View {
     public void setPresenter(MainContract.Presenter presenter) {
         mPresenter = presenter;
     }
+
+/*
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  String[] objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }*/
 }
+
+
